5411fe603a0bc4bd485e605dbd5cd848fbe8f75a
massemiso <massemiso@proton.me>
2026-04-25 14:23:23 -0300

    feat(dto): add @NotEmpty constraint to SaleRequestDto

--- src/main/java/com/massemiso/supermarket_api/dto/SaleRequestDto.java
+++ src/main/java/com/massemiso/supermarket_api/dto/SaleRequestDto.java
@@ -1,10 +1,11 @@
 package com.massemiso.supermarket_api.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record SaleRequestDto (
     @NotNull @Min(1) Long branchId,
-    @NotNull List<DetailSaleRequestDto> detailSaleRequestDtoList
+    @NotNull @NotEmpty List<DetailSaleRequestDto> detailSaleRequestDtoList
 ) { }
