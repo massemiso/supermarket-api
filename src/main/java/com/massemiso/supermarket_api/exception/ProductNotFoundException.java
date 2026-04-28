ac0aea452368feb7402fd7e420e1074a65540273
massemiso <massemiso@proton.me>
2026-04-28 15:33:15 -0300

    refactor(exception): remove character . from constructor message in ProductNotFoundException

--- src/main/java/com/massemiso/supermarket_api/exception/ProductNotFoundException.java
+++ src/main/java/com/massemiso/supermarket_api/exception/ProductNotFoundException.java
@@ -7,7 +7,7 @@ package com.massemiso.supermarket_api.exception;
  */
 public class ProductNotFoundException extends RuntimeException {
   public ProductNotFoundException(Long id) {
-    super("Product with id " + id + " not found.");
+    super("Product with id " + id + " not found");
   }
 
 }
