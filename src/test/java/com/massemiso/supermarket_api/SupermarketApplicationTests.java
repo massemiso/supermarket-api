9efa73edcba4ecfabd213908e719358e57b155bc
massemiso <massemiso@proton.me>
2026-04-29 15:23:04 -0300

    test(setup): configure and enable application context smoke test

--- src/test/java/com/massemiso/supermarket_api/SupermarketApplicationTests.java
+++ src/test/java/com/massemiso/supermarket_api/SupermarketApplicationTests.java
@@ -1,13 +1,29 @@
 package com.massemiso.supermarket_api;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
-@Disabled
+@Testcontainers
 class SupermarketApplicationTests {
+  @Container
+  static PostgreSQLContainer postgres = new PostgreSQLContainer(
+      "postgres:15");
 
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", postgres::getJdbcUrl);
+    registry.add("spring.datasource.username", postgres::getUsername);
+    registry.add("spring.datasource.password", postgres::getPassword);
+  }
+
+
+  // Checks if app connects to test container database
 	@Test
 	void contextLoads() {
 	}
