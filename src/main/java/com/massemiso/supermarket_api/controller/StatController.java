b676e360d448f321eff972277e6efec58e1e9f32
massemiso <massemiso@proton.me>
2026-05-13 14:07:24 -0300

    swagger: make StatController  docs pretty

--- src/main/java/com/massemiso/supermarket_api/controller/StatController.java
+++ src/main/java/com/massemiso/supermarket_api/controller/StatController.java
@@ -3,6 +3,8 @@ package com.massemiso.supermarket_api.controller;
 import com.massemiso.supermarket_api.dto.ApiResponse;
 import com.massemiso.supermarket_api.dto.BestSellerResponseDto;
 import com.massemiso.supermarket_api.service.StatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +24,24 @@ public class StatController {
     this.statService = statService;
   }
 
+  @Operation(
+      summary = "Get the best selling product, needs authentication"+
+          ", needs ADMIN or MANAGER role"
+  )
+  @io.swagger.v3.oas.annotations.responses.ApiResponse(
+      responseCode = "200",
+      description = "Get best selling product successfully"
+  )
+  @io.swagger.v3.oas.annotations.responses.ApiResponse(
+      responseCode = "401",
+      description = "Not authenticated",
+      content = @Content
+  )
+  @io.swagger.v3.oas.annotations.responses.ApiResponse(
+      responseCode = "403",
+      description = "Not authorized",
+      content = @Content
+  )
   @GetMapping("/best-selling-product")
   @PreAuthorize("hasAnyRole('ADMIN','MANAGER')")
   public ResponseEntity<ApiResponse<BestSellerResponseDto>> getBestSellingProduct(){
--- src/main/java/com/massemiso/supermarket_api/dto/BestSellerResponseDto.java
+++ src/main/java/com/massemiso/supermarket_api/dto/BestSellerResponseDto.java
@@ -1,9 +1,12 @@
 package com.massemiso.supermarket_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 
 public record BestSellerResponseDto(
     ProductResponseDto product,
+
+    @Schema(example = "100.0")
     BigDecimal totalRevenue
 ){
 }
